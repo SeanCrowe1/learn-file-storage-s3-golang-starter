e4aa7b0b65a21bba81d00e8b35e3fb7d769dfcdd
SeanCrowe1 <sean.crowe2002@gmail.com>
2026-01-24 20:00:48 +0000

    uploading videos to aws

--- assets.go
+++ assets.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"net/http"
 	"os"
 )
 
@@ -9,4 +12,18 @@ func (cfg apiConfig) ensureAssetsDir() error {
 		return os.Mkdir(cfg.assetsRoot, 0755)
 	}
 	return nil
+}
+
+func getAssetPath(w http.ResponseWriter) (string, error) {
+	buffer := make([]byte, 32)
+	_, err := rand.Read(buffer)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't generate URL", nil)
+	}
+
+	encoded := make([]byte, base64.RawURLEncoding.EncodedLen(len(buffer)))
+	base64.RawURLEncoding.Encode(encoded, buffer)
+
+	filename := string(encoded)
+	return filename, nil
 }
--- handler_upload_video.go
+++ handler_upload_video.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
@@ -28,7 +33,87 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't find video", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "video not owned by current user", nil)
+		return
+	}
+
 	fmt.Println("uploading video", videoID, "by user", userID)
 
 	const maxMemory int64 = 1 << 30
+	r.Body = http.MaxBytesReader(w, r.Body, maxMemory)
+
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	contentType := header.Header.Get("Content-Type")
+	if contentType == "" {
+		respondWithError(w, http.StatusBadRequest, "Missing Content-Type for video", nil)
+		return
+	}
+
+	mediaType, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Content-Type header", err)
+		return
+	}
+
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "Invalid file format", nil)
+		return
+	}
+
+	temp, err := os.CreateTemp("", "tubely-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create temporary file", err)
+		return
+	}
+	defer os.Remove(temp.Name())
+	defer temp.Close()
+
+	_, err = io.Copy(temp, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't copy to temporary file", err)
+		return
+	}
+
+	filename, err := getAssetPath(w)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't encode filename", err)
+		return
+	}
+	filename += ".mp4"
+
+	_, err = temp.Seek(0, io.SeekStart)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't reset file read pointer", err)
+		return
+	}
+
+	cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
+		Bucket:      aws.String(cfg.s3Bucket),
+		Key:         aws.String(filename),
+		Body:        temp,
+		ContentType: aws.String(mediaType),
+	})
+
+	url := "https://" + cfg.s3Bucket + ".s3." + cfg.s3Region + ".amazonaws.com/" + filename
+	video.VideoURL = &url
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video record", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
